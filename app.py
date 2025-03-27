0b114ea40d046207f144117c1416158e5755516e
vaibhav-jani <vaibhavaniljani@gmail.com>
2025-03-27 16:36:11 +0530

    Added multiline string.

--- app.py
+++ app.py
@@ -3,5 +3,16 @@ def main():
     print(strHello)  # Get the length of the string
     print("*" * len(strHello))  # Multiply by the string length
 
+    strMsg = """
+    This is a multi-line string.
+    It can be used to store a block of text.
+    It can be used to store a block of text.
+    It can be used to store a block of text.
+    It can be used to store a block of text.
+    It can be used to store a block of text.
+    """
+    print(strMsg)
+
+
 if __name__ == "__main__":
     main()
