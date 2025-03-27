fac79dd8cff1f9d5e72e755021b8bb4b2c9ec729
vaibhav-jani <vaibhavaniljani@gmail.com>
2025-03-27 15:39:03 +0530

    Fix string length calculation using len() function

---
+++ app.py
@@ -0,0 +1,7 @@
+def main():
+    strHello = "Hello, World! Welcome to the Python World!"
+    print(strHello)  # Get the length of the string
+    print("*" * len(strHello))  # Multiply by the string length
+
+if __name__ == "__main__":
+    main()
